bdaf29d2f9090b2f279cb795488c93a8049c39c1
Grandolf49 <chinmaykulkarni8@gmail.com>
2022-03-16 21:05:37 +0530

    Fix: Renamed UI Library to "uihouse"

--- uihouse/src/androidTest/java/org/mifos/mobile/uihouse/ExampleInstrumentedTest.kt
+++ uihouse/src/androidTest/java/org/mifos/mobile/uihouse/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package org.mifos.mobile.ui_library
+package org.mifos.mobile.uihouse
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("org.mifos.mobile.ui_library.test", appContext.packageName)
+        assertEquals("org.mifos.mobile.uihouse.test", appContext.packageName)
     }
 }
